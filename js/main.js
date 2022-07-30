92d428f8fa4c3fb5fc11f17bab1b10b0ce507e30
omar alfuhaidi <omaralmunssoe@gmail.com>
2022-07-30 22:21:06 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+var ready = (callback) => {
+    if (document.readyState != "loading") callback();
+    else document.addEventListener("DOMContentLoaded", callback);
+}
+
+ready(() => {
+    document.querySelector(".header").style.height = window.innerHeight + "px";
+})
+
+setTimeout(function() {
+	$('#demo-modal').modal();
+}, 500);
+
+
